93218503d719d00dc51f8766293a970fc985576e
piandas <santiagobustosp@gmail.com>
2023-08-14 22:29:49 -0500

    Ya se pusieron los detalles al darle click

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -14,19 +14,23 @@ export class PokemonDetailsComponent implements OnInit, OnDestroy {
   pokemonDetails: any;
   private destroy$ = new Subject<void>();
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.pokemonService.getPokemonDetails(1)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (data: any) => {
-          this.pokemonDetails = data;
-        },
-        (error) => {
-          console.error('Error fetching Pokemon details:', error);
-        }
-      );
+    this.route.params.subscribe(params => {
+      const pokemonName = params['name'];
+
+      this.pokemonService.getPokemonDetails(pokemonName)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(
+          (data: any) => {
+            this.pokemonDetails = data;
+          },
+          (error) => {
+            console.error('Error fetching Pokemon details:', error);
+          }
+        );
+    });
   }
 
   ngOnDestroy() {
