6e30ce2e723675ab22ef8c17583a9de4aac0a35f
piandas <santiagobustosp@gmail.com>
2023-08-15 11:25:52 -0500

    Arreglos para que se vea bien

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,39 +1,12 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { PokemonService } from '../../services/pokemon.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
   styleUrls: ['./pokemon-details.component.scss']
 })
-export class PokemonDetailsComponent implements OnInit, OnDestroy {
+export class PokemonDetailsComponent {
   @Input() pokemonDetails: any;
-  private destroy$ = new Subject<void>();
 
-  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      const pokemonName = params['name'];
-
-      this.pokemonService.getPokemonDetails(pokemonName)
-        .pipe(takeUntil(this.destroy$))
-        .subscribe(
-          (data: any) => {
-            this.pokemonDetails = data;
-          },
-          (error) => {
-            console.error('Error fetching Pokemon details:', error);
-          }
-        );
-    });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+  constructor() {}
 }
